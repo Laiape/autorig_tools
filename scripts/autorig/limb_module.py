d11ae49ca80fa321eaa830fc0d24f78cbec758c7
Laia Peris <laiaperis3@gmail.com>
2025-08-21 18:04:11 +0200

    limb module started, ribbons wip

--- scripts/autorig/limb_module.py
+++ scripts/autorig/limb_module.py
@@ -2,11 +2,11 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as om
 import os
 from importlib import reload
-from scripts.utils import data_export
+from scripts.utils import data_manager
 from scripts.utils import guides_manager
 from scripts.utils import curve_tool
 
-reload(data_export)
+reload(data_manager)
 reload(guides_manager)
 reload(curve_tool)
 
@@ -19,25 +19,78 @@ class LimbModule(object):
         self.guides_path = os.path.join(self.relative_path, "guides", "character_guides.guides")
         self.curves_path = os.path.join(self.relative_path, "curves", "curves.json")
 
-        
+        self.modules = data_manager.DataExport().get_data("basic_structure", "modules_GRP")
+        self.skel_grp = data_manager.DataExport().get_data("basic_structure", "skel_GRP")
+        self.masterwalk_ctl = data_manager.DataExport().get_data("basic_structure", "masterwalk_ctl")
 
-        self.module = "Limb"
+
+        self.module = "limb"
 
     def make(self, side):
 
         self.side = side
 
-        self.module_trn = cmds.createNode("transform", name=f"{self.side}_{self.module}Module_GRP", ss=True, p=self.modules_grp)
-        self.controllers_grp = cmds.createNode("transform", name=f"{self.side}_{self.module}Controllers_GRP", ss=True, p=self.masterWalk_ctl)
+        self.module_trn = cmds.createNode("transform", name=f"{self.side}_{self.module}Module_GRP", ss=True, p=self.modules)
+        self.controllers_grp = cmds.createNode("transform", name=f"{self.side}_{self.module}Controllers_GRP", ss=True, p=self.masterwalk_ctl)
         self.skeleton_grp = cmds.createNode("transform", name=f"{self.side}_{self.module}Skinning_GRP", ss=True, p=self.skel_grp)
 
+    def lock_attributes(self, ctl, attrs):
 
-    def blend_chains(guides):
+        """
+        Lock and hide attributes on a controller.
+        Args:
+            ctl (str): The name of the controller.
+            attrs (list): A list of attributes to lock and hide.
+        """
+        
+        for attr in attrs:
+            cmds.setAttr(f"{ctl}.{attr}", lock=True, keyable=False, channelBox=False)
+    
+    def blend_chains(self):
 
-        fk_chain = []
-        ik_chain = []
-        blend_chain = []
-        names = ["Fk", "Ik", "Blend"]
+        self.settings_node, self.settings_ctl = curve_tool.create_controller(name=f"{self.side}_armSettings", offset=["GRP"])
+        self.lock_attributes(self.settings_ctl, ["translateX", "translateY", "translateZ", "rotateX", "rotateY", "rotateZ", "scaleX", "scaleY", "scaleZ", "visibility"])
+        cmds.matchTransform(self.settings_node[0], self.settings_loc, pos=True, rot=True)
+        cmds.delete(self.settings_loc)
+        cmds.addAttr(self.settings_ctl, longName="Ik_Fk", attributeType="float", defaultValue=0, minValue=0, maxValue=1, keyable=True)
+        cmds.parent(self.settings_node[0], self.controllers_grp)
+
+        self.pair_blends = []
+        self.fk_chain = []
+        self.ik_chain = []
+
+        for joint in self.limb_chain:
+
+            pair_blend = cmds.createNode("pairBlend", name=joint.replace("JNT", "PBL"), ss=True)
+            cmds.connectAttr(f"{self.settings_ctl}.Ik_Fk", f"{pair_blend}.weight")
+
+            cmds.select(clear=True)
+            fk_joint = cmds.joint(name=joint.replace("_JNT", "Fk_JNT"))
+            cmds.makeIdentity(fk_joint, apply=True, translate=True, rotate=True, scale=True, normal=False)
+            cmds.parent(fk_joint, self.module_trn)
+
+            cmds.select(clear=True)
+            ik_joint = cmds.joint(name=joint.replace("_JNT", "Ik_JNT"))
+            cmds.matchTransform(ik_joint, joint, pos=True, rot=True)
+            cmds.makeIdentity(ik_joint, apply=True, translate=True, rotate=True, scale=True, normal=False)
+
+            if self.ik_chain:
+                cmds.parent(ik_joint, self.ik_chain[-1])
+
+            self.fk_chain.append(fk_joint)
+            self.ik_chain.append(ik_joint)
+            self.pair_blends.append(pair_blend)
+
+            cmds.connectAttr(f"{fk_joint}.translate", f"{pair_blend}.inTranslate2")
+            cmds.connectAttr(f"{ik_joint}.translate", f"{pair_blend}.inTranslate1")
+            cmds.connectAttr(f"{fk_joint}.rotate", f"{pair_blend}.inRotate2")
+            cmds.connectAttr(f"{ik_joint}.rotate", f"{pair_blend}.inRotate1")
+            cmds.connectAttr(f"{pair_blend}.outTranslate", f"{joint}.translate")
+            cmds.connectAttr(f"{pair_blend}.outRotate", f"{joint}.rotate")
+
+        # cmds.parent(self.fk_chain[0], self.module_trn)
+        cmds.parent(self.ik_chain[0], self.module_trn)
+
 
 
 
@@ -58,17 +111,27 @@ class ArmModule(LimbModule):
 
     def load_guides(self):
 
-        self.arm_chain = guides_manager.guide_import(joint_name=f"{self.side}_shoulder_JNT", all_descendents=True, filePath=self.guides_path)
-        
+        """
+        Load guides from arm
+        """
 
+        self.limb_chain = guides_manager.get_guides(joint_name=f"{self.side}_shoulder_JNT")
+        self.settings_loc = guides_manager.get_guides(joint_name=f"{self.side}_armSettings_LOC")[0]
+        
 
 class LegModule(LimbModule):
 
     def make(self, side):
 
         self.side = side
-
+        self.module = "leg"
+        super(LegModule, self).make(side)
 
     def load_guides(self):
 
-        pass
+        """
+        Load guides from leg
+        """
+
+        self.limb_chain = guides_manager.get_guides(joint_name=f"{self.side}_hip_JNT")
+        self.settings_loc = guides_manager.get_guides(joint_name=f"{self.side}_legSettings_LOC")[0]
--- scripts/autorig/utilities/ribbon.py
+++ scripts/autorig/utilities/ribbon.py
@@ -7,6 +7,7 @@ reload(core)
 
 OPEN = "open"
 PERIODIC = "periodic"
+AXIS_VECTOR = {"x": (1,0,0), "y": (0,1,0), "z": (0,0,1)}
 KNOT_TO_FORM_INDEX = {OPEN : om.MFnNurbsCurve.kOpen, PERIODIC : om.MFnNurbsCurve.kPeriodic}
 
 
@@ -60,8 +61,6 @@ def de_boor_ribbon(cvs, controllers_grp = [], aim_axis="x", up_axis="y", num_joi
                 cmds.matchTransform(grp, cv, pos=True, rot=True, scl=False)
             controllers_groups.append(grp)
             ctls.append(ctl)          
-            
-            
 
     num_cvds = len(cvs)
     original_cvs = cvs[:]
@@ -69,92 +68,176 @@ def de_boor_ribbon(cvs, controllers_grp = [], aim_axis="x", up_axis="y", num_joi
     d = num_cvds - 1 if d is None else d
 
     if kv_type == OPEN:
+
+        print("Open")
         
         kv, _ = core.knot_vector(OPEN, cvs, d)
 
+        m_kv = kv[1:-1]  # Remove the first and last knots for open curves
+        m_cvs = cvs[:]
+
     else:  # kv_type is PERIODIC
+        print("Periodic")
         m_cvs = [i % len(cvs) for i in range(len(cvs))]
         for i in range(d):
             m_cvs.append(m_cvs[i])
 
         m_kv_len = len(m_cvs) + d + 1
         m_kv_interval = 1 / (m_kv_len - 2 * (d - 1))
-        m_kv = [
-            t / (m_kv_len - 1)
-            for t in range(m_kv_len)
-        ]
-
+        m_kv = [-m_kv_interval * (d - 1) * (1 - t / (m_kv_len - 1)) +
+        (1 + m_kv_interval * (d - 1)) * t / (m_kv_len - 1) for t in range(m_kv_len)]
+        
         kv, cvs = core.knot_vector(PERIODIC, cvs, d)
 
-        m_cv_pos = om.MPointArray(
-            [cmds.xform(obj, q=True, ws=True, t=True) for obj in m_cvs]
-        )
+    m_cv_pos = om.MPointArray(
+        [cmds.xform(obj, q=True, ws=True, t=True) for obj in m_cvs]
+    )
 
-        form = KNOT_TO_FORM_INDEX[kv_type]
-        is_2d = False
-        rational = True
-        data_creator = om.MFnNurbsCurveData()
-        parent = data_creator.create()
+    form = KNOT_TO_FORM_INDEX[kv_type]
+    is_2d = False
+    rational = True
+    data_creator = om.MFnNurbsCurveData()
+    parent = data_creator.create()
 
-        crv_fn = om.MFnNurbsCurve() # Create a NURBS curve function set
-        crv_fn.create(m_cv_pos, m_kv, d, form, is_2d, rational)#, parent)
+    crv_fn = om.MFnNurbsCurve() # Create a NURBS curve function set
+    crv_fn.create(m_cv_pos, m_kv, d, form, is_2d, rational, parent)
 
 
-        if parameter_length: # Calculate the parameters for the controllers based on the curve length or evenly spaced
+    if parameter_length: # Calculate the parameters for the controllers based on the curve length or evenly spaced
 
-            crv_length = crv_fn.length()
-            params = []
+        crv_length = crv_fn.length()
+        params = []
 
-            for i in range(num_joints):
+        for i in range(num_joints):
 
-                # Calculate the parameter based on the length of the curve
+            # Calculate the parameter based on the length of the curve
 
-                sample_len = crv_length * (i / (num_joints - 1))
-                t = crv_fn.findParamFromLength(sample_len)
-                params.append(t)
-        else:
+            sample_len = crv_length * (i / (num_joints - 1))
+            t = crv_fn.findParamFromLength(sample_len)
+            params.append(t)
+    else:
 
-            params = [i/(num_joints - 1) for i in range(num_joints)]
+        params = [i/(num_joints - 1) for i in range(num_joints)]
+
+    if kv_type == PERIODIC:
+
+        params = [(kv[d + 1] * (d * 0.5 + 0.5)) * (1 - t) + t * (1 - kv[d + 1] * (d * 0.5 - 0.5))
+                for i, t in enumerate(params)]
+
+    parent_offsets = []
+    translation_offsets = []
+    
+    for i, ctl in enumerate(ctls):
+
+        par_off = cmds.createNode("multMatrix", n=f"{name}_parentOffset_{i}_MM")
+        cmds.connectAttr(f"{ctl}.worldMatrix", f"{par_off}.matrixIn[0]")
+        cmds.connectAttr(f"{controllers_groups[0]}.worldInverseMatrix", f"{par_off}.matrixIn[1]")
+
+        parent_offsets.append(f"{par_off}.matrixSum")
+        
+        trans_off = cmds.createNode("decomposeMatrix", n=f"{name}_transOffset0{i}_DM")
+        compose_matrix = cmds.createNode("composeMatrix", n=f"{name}_0{i}_CM")
+        cmds.connectAttr(f"{par_off}.matrixSum", f"{trans_off}.inputMatrix")
+        cmds.connectAttr(f"{trans_off}.outputTranslate", f"{compose_matrix}.inputTranslate")
+
+        translation_offsets.append(f"{compose_matrix}.outputMatrix")
+
+
+    jnts = []
+
+    for i, param in enumerate(params):
+        
+        cmds.select(clear=True)
+        jnt = cmds.joint(n = f"{name}0{i}_JNT")
+        cmds.parent(jnt, jnts_grp)
+        cmds.setAttr(f"{jnt}.jo", 0, 0, 0)
+        cmds.xform(jnt, m=om.MMatrix.kIdentity)
+
+        jnts.append(jnt)
+
+        wts = core.de_boor(len(cvs), d, param, kv)
+
+        if kv_type == OPEN:
+
+            consolidated_weights = get_consolidated_wts(wts, original_cvs, cvs)
+        
+        position = create_weight_add_matrix(translation_offsets, wts, f"{name}0{i}Trans_WAM", tol)
+
+        tangent_param = tangent_offset + param
+        aim_vector = om.MVector(AXIS_VECTOR[aim_axis])
+
+        if tangent_param > 1:
+            tangent_param = param - 2 * tangent_offset
+            aim_vector *= -1
+
+        tangent_wts = core.de_boor(len(cvs), d, tangent_param, kv, tol)
 
         if kv_type == PERIODIC:
 
-            params = [(kv[d + 1] * (d * 0.5 + 0.5)) * (1 - t) + t * (1 - kv[d + 1] * (d * 0.5 - 0.5))
-                    for i, t in enumerate(params)]
+            tangent_wts = get_consolidated_wts(tangent_wts, original_cvs, cvs)
 
-        parent_offsets = []
-        translation_offsets = []
-        
-        for i, ctl in enumerate(ctls):
+        tangent = create_weight_add_matrix(translation_offsets, tangent_wts, f"{name}0{i}Tan_WAM", tol)
 
-            par_off = cmds.createNode("multMatrix", n=f"{name}_parentOffset_{i}_MM")
-            cmds.connectAttr(f"{ctl}.worldMatrix", f"{par_off}.matrixIn[0]")
-            cmds.connectAttr(f"{controllers_groups[0]}.worldInverseMatrix", f"{par_off}.matrixIn[1]")
+        temp = cmds.createNode("transform")
+        ori_con = cmds.orientConstraint(original_cvs, temp)[0]
+        cmds.setAttr(f"{ori_con}.interpType", 2)
+        for j, wt in enumerate(wts):
+            cmds.setAttr(f"{ori_con}.{original_cvs[j]}W{j}", wt)
 
-            parent_offsets.append(f"{par_off}.matrixSum")
-            
-            trans_off = cmds.createNode("decomposeMatrix", n=f"{name}_transOffset_{i}_DM")
-            cmds.connectAttr(f"{par_off}.matrixSum", f"{trans_off}.inputMatrix")
+        up = create_weight_add_matrix(parent_offsets, wts, f"{name}_up0{i}_WAM", tol=tol)
 
-            translation_offsets.append(f"{trans_off}.outputTranslate")
+        temp_mat = om.MMatrix(cmds.getAttr(f"{temp}.worldMatrix"))
+        up_inverse = om.MMatrix(cmds.getAttr(f"{up}.matrixSum")).inverse()
+        up_off_val = temp_mat * up_inverse
+
+        up_off = cmds.createNode("multMatrix", n=f"{name}_upOffset0{i}_MM", ss=True)
+        cmds.setAttr(f"{up_off}.matrixIn[0]", list(up_off_val), type="matrix")
+        cmds.connectAttr(f"{up}.matrixSum", f"{up_off}.matrixIn[1]")
+
+        cmds.delete(temp)
+
+        aim = cmds.createNode("aimMatrix", n=f"{name}_pointOnCurve0{i}_AMX", ss=True)
+        cmds.connectAttr(f"{position}.matrixSum", f"{aim}.inputMatrix")
+        cmds.connectAttr(f"{tangent}.matrixSum", f"{aim}.primaryTargetMatrix")
+        cmds.connectAttr(f"{up}.matrixSum", f"{aim}.secondaryTargetMatrix")
+        cmds.setAttr(f"{aim}.primaryInputAxis", *aim_vector)
+        cmds.setAttr(f"{aim}.secondaryInputAxis", *AXIS_VECTOR[up_axis])
+        cmds.setAttr(f"{aim}.secondaryMode", 2)
+        cmds.setAttr(f"{aim}.secondaryTargetVector", *AXIS_VECTOR[up_axis])
+
+        cmds.connectAttr(f"{aim}.outputMatrix", f"{jnt}.offsetParentMatrix")
 
 
-        jnts = []
 
-        for i, param in enumerate(params):
-            
-            cmds.select(clear=True)
-            jnt = cmds.joint(n = f"{name}0{i}_JNT")
-            cmds.parent(jnt, jnts_grp)
-            cmds.setAttr(f"{jnt}.jo", 0, 0, 0)
-            cmds.xform(jnt, m=om.MMatrix.kIdentity)
+    return jnts
 
-            jnts.append(jnt)
-
-        return jnts
-    
     """
     import ribbon
     from importlib import reload
     reload(ribbon)
     ribbon.de_boor_ribbon(cmds.ls(sl=True))
     """
+
+def get_consolidated_wts(wts, original_cvs, cvs):
+     
+    consolidated_weights = {cv : 0 for cv in original_cvs}
+
+    for j, wt in enumerate(wts):
+        consolidated_weights[cvs[j]] += wt
+
+    return [consolidated_weights[cv] for cv in original_cvs]
+
+def create_weight_add_matrix(matrix_attrs, wts, name, tol=0.000001):
+
+    wam = cmds.createNode("wtAddMatrix", n=name)
+
+    for matrix_attr, wt, i in zip(matrix_attrs, wts, range(len(matrix_attrs))):
+
+        if wt < tol:
+
+            continue
+
+        cmds.connectAttr(matrix_attr, f"{wam}.wtMatrix[{i}].matrixIn")
+        cmds.setAttr(f"{wam}.wtMatrix[{i}].weightIn", wt)
+
+    return wam 
