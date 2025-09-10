a17ca8a6b62b33f9e8636d3bae73e8723e016a2e
laia.peris <laiaperis3@gmail.com>
2025-09-10 09:16:07 +0200

    Refactor userSetup script to encapsulate command port setup and improve auto rig UI initialization

--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,9 +1,16 @@
 import maya.utils as mu
 import maya.cmds as cmds
 
-if not cmds.commandPort(":4434", query=True):
-    cmds.commandPort(name=":4434")
-if not cmds.commandPort("localhost:7001", query=True):
-    cmds.commandPort(name="localhost:7001")
+def vs_code_ports():
+    if not cmds.commandPort(":4434", query=True):
+        cmds.commandPort(name=":4434")
+    if not cmds.commandPort("localhost:7001", query=True):
+        cmds.commandPort(name="localhost:7001")
 
-mu.executeDeferred("from ui import auto_rig_UI; import importlib; importlib.reload(auto_rig_UI)")
+def init_auto_rig_UI():
+    try:
+        from ui import auto_rig_UI
+        auto_rig_UI()
+    except Exception as e:
+        cmds.warning("Could not load auto_rig_UI: {}".format(e))
+mu.executeDeferred(init_auto_rig_UI)
